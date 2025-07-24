b433416bf35c06e89cbf1f5ed4c96968c87ac2fb
joaoatilamonteiro <steeljaw2006@gmail.com>
2025-07-24 12:46:42 -0300

    Conexão do código com a planilha otimizada Função cadastrar_filmes melhor trabalhada separando as avaliacoes em uma planilha sozinha e deixando as outras informações na planilha filmes
    Organização do esqueleto do código

--- cinema.py
+++ cinema.py
@@ -7,11 +7,13 @@ sequencias = []
 
 
 avaliacoes = r"avaliacoes.csv"
-filmes = "filmes.csv"
+colunas_avaliacoes = ['Título', 'Estrelas', 'Número avaliações']
+filmes = r"filmes.csv"
+lista_colunas = ['Título', 'Ano', 'Gênero']
 
 def mostra_menu(total):
   print("""
-  ***Qual opção deseja escolher?***\n
+  ***Qual opção deseja escolher?***
   1 - cadastrar filme
   2 - Avaliar filme
   3 - Consultar Filme por Título
@@ -21,49 +23,82 @@ def mostra_menu(total):
   7 - Sair""")
 
 
-def carregar_filmes():
-  filmes_carregados = pad.read_csv(filmes, sep = ",")
-  #lista_colunas = filmes_carregados.columns.tolist()
-  return filmes_carregados
+def carrega_arquivos(caminho, coluna_padrao):
+  dataframe = pad.read_csv(caminho, sep = ",")
+  print(f"arquivo {caminho} carregado")
+  return dataframe
 
-def carregar_avaliacoes():
-  avaliacoes_carregadas = pad.read_csv(avaliacoes, sep = ",")
-  #media = (avaliacoes_carregadas.groupby("Título")["Estrelas"].mean().round(2))
-
-def cadastrar_filme(filmes_carregados):  # Agora recebe o DataFrame corretamente
+def cadastrar_filme(filmes_carregados, avaliacoes_carregadas):  # Agora recebe o DataFrame corretamente
 
   print("""*********** SysFilmes ***********
   ******* Cadastrando Filme *******
   *********************************
   """)
 
-  lista_colunas = ['Título', 'Ano', 'Gênero']
-
-  novo_filme = {}
+  novo_filme, novo_filme_estrela = {}, {}
   for coluna in lista_colunas:
     valor_digitado = input(f"{coluna}: ").strip()
+
     if coluna == "Ano":
       novo_filme[coluna] = int(valor_digitado)
     else:
       novo_filme[coluna] = valor_digitado
 
-  novo_filme['estrelas'] = 0.0
-  novo_filme['numero_avaliacoes'] = 0
+
+  novo_filme_estrela['Título'] = novo_filme.get("Título")
+  novo_filme_estrela['Estrelas'] = 0.0
+  novo_filme_estrela['Número avaliações'] = 0
+
   filmes_add = pad.concat([filmes_carregados, pad.DataFrame([novo_filme])], ignore_index=True)
-  filmes_add.to_csv("filmes.csv", sep=",", index=False)
+  filmes_add.to_csv(filmes, sep=",", index=False)
+
+  filmes_add_estrela = pad.concat([avaliacoes_carregadas, pad.DataFrame([novo_filme_estrela])], ignore_index=True)
+  filmes_add_estrela.to_csv(avaliacoes, sep=",", index=False)
+
+
   print("\nFilme cadastrado com sucesso!")
   print("\n[**Tecle enter para continuar**]")
   input()
+
+
+def avaliar_filme(filmes):
+  pass
+
+def lista_estrelas(num, filmes):
+  #solicita número mínimo de estrelas
+  #lista os filmes com as estrelas maior ou igual ao valor dado
+  pass
+
+def lista_todos(filmes):
+  #mostra todos os filmes com suas respectivas informações
+  pass
 
 opcao = -1
 while True:
 
   mostra_menu(opcao)
-  opcao = int(input('\n'))
+  opcao = int(input('Digite a opção: '))
   if opcao == 1:
-    df_filmes = carregar_filmes()
-    cadastrar_filme(df_filmes)
-  if opcao == 7:
+    df_filmes = carrega_arquivos(filmes, lista_colunas)
+    df_avaliacoes_atuais = carrega_arquivos(avaliacoes, colunas_avaliacoes)
+    cadastrar_filme(df_filmes,df_avaliacoes_atuais)
+
+  elif opcao == 2:
+    pass
+
+  elif opcao == 3:
+    pass
+
+  elif opcao == 4:
+    pass
+
+  elif opcao == 5:
+    pass
+
+  elif opcao == 6:
+    pass
+  elif opcao == 7:
+    print("Encerrado o programa...")
     break
 
 
