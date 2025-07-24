76c6143aa1a66a4973f3721139f843ebbe17121d
joaoatilamonteiro <steeljaw2006@gmail.com>
2025-07-23 21:05:38 -0300

    Conexão do código com a planilha função mostra_menu funcional Falta criar ainda as funções   2 - Avaliar filme   3 - Consultar Filme por Título   4 - Listar Filmes por Gênero   5 - Listar filmes por estrela   6 - Listar todos os filmes

--- cinema.py
+++ cinema.py
@@ -1,106 +1,72 @@
 #fazer leitura de csvs avaliacoes e filmes para poder ser usada na tabela
+import pandas as pad
+import os
 
-def cria_filme():
-  """
-  Pede ao usuário as informações de um novo filme e retorna em um dicionário com os dados.
 
-  Retorno:
-  dict: Dicionário com os dados do filme (título, ano, gênero, estrelas e avaliações).
-  """
+sequencias = []
+
+
+avaliacoes = r"avaliacoes.csv"
+filmes = "filmes.csv"
+
+def mostra_menu(total):
+  print("""
+  ***Qual opção deseja escolher?***\n
+  1 - cadastrar filme
+  2 - Avaliar filme
+  3 - Consultar Filme por Título
+  4 - Listar Filmes por Gênero
+  5 - Listar filmes por estrela
+  6 - Listar todos os filmes
+  7 - Sair""")
+
+
+def carregar_filmes():
+  filmes_carregados = pad.read_csv(filmes, sep = ",")
+  #lista_colunas = filmes_carregados.columns.tolist()
+  return filmes_carregados
+
+def carregar_avaliacoes():
+  avaliacoes_carregadas = pad.read_csv(avaliacoes, sep = ",")
+  #media = (avaliacoes_carregadas.groupby("Título")["Estrelas"].mean().round(2))
+
+def cadastrar_filme(filmes_carregados):  # Agora recebe o DataFrame corretamente
+
   print("""*********** SysFilmes ***********
-******* Cadastrando Filme *******
-*********************************
-""")
-  #nessa função os dados add aqui serao add nas suas respectivas tabelas de avaliacoes como filmes
-  filme = {}
-  filme["Título"] = input("titulo: ").strip()
-  filme["Ano"] = int(input("ano: ").strip())
-  filme["Gênero"] = input("genero: ").strip()
-  filme["Estrelas"] = 0.0
-  filme["numero de avaliações"] = 0
-  print("\n[**Tecle enter para voltar ao Menu Principal**]")
-  input()
-  return filme
+  ******* Cadastrando Filme *******
+  *********************************
+  """)
 
-def mostra_filme(filme):
-  """
-  Exibe os detalhes de um filme.
+  lista_colunas = ['Título', 'Ano', 'Gênero']
 
-  Parâmetros:
-  filme (dict): Dicionário contendo as informações de um filme.
-  """
-  #fazer um print que mostre as colunas da tabela filmes, sendo as colunas titulo ano e genero
-  print(f"Título: {filme['titulo']}")
-  print(f"Ano: {filme['ano']}")
-  print(f"Gênero: {filme['genero']}")
-  print(f"Estrelas: {filme['estrelas']}")
-  print(f"Número de avaliações: {filme['avaliacoes']}")
-  print("\n*********************************")
-def lista_todos(filmes):
-  """
-  Exibe a lista completa de todos os filmes cadastrados.
-
-  Parâmetros:
-  filmes (list): Lista contendo os filmes cadastrados.
-  """
-  print("""*********** SysFilmes ***********
-*** Listando Filmes ***
-*********************************""")
-
-  if filmes:
-      for filme in filmes:
-          mostra_filme(filme)
-  else:
-      print("Nenhum filme cadastrado.")
-
-  print("[**Tecle enter para voltar ao Menu Principal**]")
-  input()
-
-def lista_genero(genero, filmes):
-  """
-  Exibe os filmes do gênero definido pelo usuário.
-  #ler a tabela filmes, especificamente coluna genero
-  Parâmetros:
-  genero (str): Gênero a ser buscado.
-  filmes (list): Lista de filmes cadastrados.
-  """
-  print("""*********** SysFilmes ***********
-      *** Listando Filmes por Gênero **
-      *********************************""")
-  encontrado = False
-
-  for filme in filmes:
-    if filme["Gênero"].lower() == genero.lower():
-      mostra_filme(filme)
-      encontrado = True
-
+  novo_filme = {}
+  for coluna in lista_colunas:
+    valor_digitado = input(f"{coluna}: ").strip()
+    if coluna == "Ano":
+      novo_filme[coluna] = int(valor_digitado)
     else:
-      print("Nenhum filme encontrado com esse gênero.")
+      novo_filme[coluna] = valor_digitado
 
-  print("[**Tecle enter para voltar ao Menu Principal**]")
+  novo_filme['estrelas'] = 0.0
+  novo_filme['numero_avaliacoes'] = 0
+  filmes_add = pad.concat([filmes_carregados, pad.DataFrame([novo_filme])], ignore_index=True)
+  filmes_add.to_csv("filmes.csv", sep=",", index=False)
+  print("\nFilme cadastrado com sucesso!")
+  print("\n[**Tecle enter para continuar**]")
   input()
 
-def lista_estrelas(min_estrelas, filmes):
-  """
-  Exibe os filmes com estrelas maiores ou iguais ao valor informado.
-#na tabela avaliações tem duas colunas filme e avaliação entao aqui usaria somente avaliação
-#talvez colocar um sistema que mostra as estrelas sem ser em numero
+opcao = -1
+while True:
 
-  Parâmetros:
-  min_estrelas (float): Valor mínimo de estrelas.
-  filmes (list): Lista de filmes cadastrados.
-  """
-  print("""*********** SysFilmes ***********
-  ** Listando Filmes por Estrelas *
- *********************************""")
-  encontrado = False
-  for filme in filmes:
-    if filme["Estrelas"] >= min_estrelas:
-      mostra_filme(filme)
-      encontrado = True
+  mostra_menu(opcao)
+  opcao = int(input('\n'))
+  if opcao == 1:
+    df_filmes = carregar_filmes()
+    cadastrar_filme(df_filmes)
+  if opcao == 7:
+    break
 
-    if not encontrado:
-      print("Nenhum filme com essa quantidade de estrelas ou mais.")
 
-    print("[**Tecle enter para voltar ao Menu Principal**]")
-    input()
+
+
+
